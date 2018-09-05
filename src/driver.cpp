053c1b3acc745368b2359e923d89b28ce97f86b4
Nova Rover Team <bensteer0@gmail.com>
2018-09-05 21:18:17 +1000

    Changed tabs to spaces

--- src/driver.cpp
+++ src/driver.cpp
@@ -4,14 +4,14 @@
 // Last modified by: Ben Steer
 //
 // Description:
-//	The driver node receives driving commands from the rover_manager
+//  The driver node receives driving commands from the rover_manager
 //  node and interprets these to communicate with the rover's motor 
 //  controllers to achieve the desired motion. If using the simulator,
-//	commands will be given to the virtual rover's controllers instead.
+//  commands will be given to the virtual rover's controllers instead.
 //
 // Message to Johnny and Cheston:
-//	Please take note of and try to adhere to the programming practices
-//	shown in this file. E.g. indentation, commenting, function 
+//  Please take note of and try to adhere to the programming practices
+//  shown in this file. E.g. indentation, commenting, function 
 //  descriptions, meaningful naming conventions, line spacing, etc.
 //  You will thank yourself later! :P
 //
@@ -35,12 +35,12 @@ using namespace std;
 //--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
 // drive_cmd_cb():
 //
-//		Callback function for receiving driving commands from the 
-// 		rover_manager.
+//    Callback function for receiving driving commands from the 
+//    rover_manager.
 //--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
 void drive_cmd_cb(const nova_common::DriveCmd::ConstPtr& msg)
 {   
-	// TODO do something with the drive commands
+  // TODO do something with the drive commands
 }
 
 
@@ -59,7 +59,7 @@ void SigintHandler(int sig)
 //--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
 // main():
 //
-//		Main function. This will run first.
+//    Main function. This will run first.
 //--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
 int main(int argc, char **argv)
 {
@@ -69,7 +69,7 @@ int main(int argc, char **argv)
 
   // Declare subscriber to drive cmds
   ros::Subscriber drive_cmd_sub = 
-		n.subscribe("/rover_manager/drive_cmd", 1, drive_cmd_cb); 
+    n.subscribe("/rover_manager/drive_cmd", 1, drive_cmd_cb); 
 
   // TODO Declare service clients for simulator rover stuff
 
@@ -79,31 +79,31 @@ int main(int argc, char **argv)
   bool simulator = true; 
 
   if (simulator)
-	{
-		// TODO Initialise the service clients you declared above. Note the
-		// difference between declaring and initialising. Above, you want 
-		// to simply declare that the clients exist but give them no value.
-		// Here, you want to actually create the client. The point of this 
-		// is to avoid scope issues (if you both declare and initialise the
-		// client within this "if" statement, it will be destroyed once the
-		// program exits the statement).
-	}
+  {
+    // TODO Initialise the service clients you declared above. Note the
+    // difference between declaring and initialising. Above, you want 
+    // to simply declare that the clients exist but give them no value.
+    // Here, you want to actually create the client. The point of this 
+    // is to avoid scope issues (if you both declare and initialise the
+    // client within this "if" statement, it will be destroyed once the
+    // program exits the statement).
+  }
 
   while (ros::ok()) // Main loop
   {
-		if (simulator)
-		{
-    	// TODO Use the received drive commands to direct the virtual 
-			// rover via service calls.
-		}
-		else
-		{
-			// Do nothing for now. Later we will communicate with the
-			// real rover.
-		}
+    if (simulator)
+    {
+      // TODO Use the received drive commands to direct the virtual 
+      // rover via service calls.
+    }
+    else
+    {
+      // Do nothing for now. Later we will communicate with the
+      // real rover.
+    }
 
-  	ros::spinOnce();   // Messages are received and callbacks called
-  	loop_rate.sleep(); // Sleep for the specified rate
+    ros::spinOnce();   // Messages are received and callbacks called
+    loop_rate.sleep(); // Sleep for the specified rate
   }
 
   return 0;
