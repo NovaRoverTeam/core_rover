459eaf273b4fd587a05fd0cc32bc602af4557199
Evan <ecra93@gmail.com>
2019-04-02 22:34:55 +1100

    Topic names consistent with Nova

--- scripts/play_uav_bag.sh
+++ scripts/play_uav_bag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosbag play imu-gps-test-small-fix.bag /px4/raw/gps:=/gps_data /px4/raw/imu:=/imu_data
+rosbag play imu-gps-test-small-fix.bag /px4/raw/gps:=/pioneer3at/gps/values /px4/raw/imu:=/pioneer3at/imu/values
