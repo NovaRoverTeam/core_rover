499fb4539c493e37aa453d8e89b6185e6d7506c1
Kieren Pinto <kieren.pinto@hotmail.com>
2019-04-12 01:03:33 +1000

    Autonomous State Machine

--- scripts/tennis_loc.py
+++ scripts/tennis_loc.py
@@ -9,7 +9,11 @@ import socket
 import sys
 import os
 from std_msgs.msg import String
-
+#--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
+# getAutoMode(): Retrieve Mode from parameter server.
+#--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
+def getAutoMode():
+    return rospy.get_param('/core_rover/autonomous_mode')
 def X_Zone(x):
     zones = ('left','centre','right')
     centre_border = (426,853)
@@ -28,7 +32,7 @@ def Drive_Forward(w,h,threshold):
         print(me)
     avg = sum([w,h])/float(2)
     return avg<threshold
-    
+
 def DriverMsg(data,steer_limit,rpm_limit):
     steer = {'left':-steer_limit,'centre':0,'right':steer_limit}
     cmd = DriveCmd()
@@ -39,7 +43,7 @@ def DriverMsg(data,steer_limit,rpm_limit):
     if (direction == 'centre' and Drive_Forward(data[2],data[3],200)):
         cmd.rpm = rpm_limit
     else:
-        cmd.rpm = 0     
+        cmd.rpm = 0
     return cmd
 
 def tennis_loc():
@@ -53,18 +57,22 @@ def tennis_loc():
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.bind(server_address)
     sock.listen(1)
-    ball_pub  = rospy.Publisher("/core_rover/navigation/tennis_loc", DriveCmd, queue_size=10)
+    # ball_pub  = rospy.Publisher("/core_rover/navigation/tennis_loc", DriveCmd, queue_size=10)
+    ball_pub  = rospy.Publisher("/core_rover/driver/drive_cmd", DriveCmd, queue_size=10)
+    status_pub = rospy.Publisher("/core_rover/navigation/tennis_stat", String, queue_size=10)
     rospy.init_node('tennis_loc', anonymous=True)
-    rate = rospy.Rate(30) # Loop rate in Hz
+    rate_active = rospy.Rate(30) # Loop rate in Hz
+    rate_inactive = rospy.Rate(1)
     rospy.loginfo("tennis ball location node started")
-
+    autonomous_mode = 'Off'
     while not rospy.is_shutdown():
     	connection, client_address = sock.accept()
-    	while True:
+    	while getAutoMode not 'Off':
         	steer_limit = rospy.get_param('steer_limit')
         	rpm_limit   = rospy.get_param('rpm_limit')
             data = connection.recv(100) # 1 Byte per character
             if data:
+                status_pub.publish("Found")
                 #rospy.loginfo(data)
                 data_parsed = ast.literal_eval(data)
                 #print(data_parsed)
@@ -73,8 +81,11 @@ def tennis_loc():
                 rospy.loginfo('publishing: ' + str(msg) + str(data_parsed))
                 ball_pub.publish(msg)
             else:
+                status_pub.publish("Lost")
                 connection.close()
                 break;
+            rate_active.sleep()
+        rate_inactive.sleep()
 
 
 if __name__ == '__main__':
