79f9cd06bc192e52cfc8cca4b2e993e359a07f1f
jmcrobbie <jackmcrobbie@hotmail.com>
2019-04-12 16:59:01 +1000

    init

---
+++ scripts/steeringPID.py
@@ -0,0 +1,30 @@
+class steeringPIDL:
+    self.kp = 0.1 
+    self.kI = 0.3
+    self.kd = 0.00001
+    self.kiMax = 1
+    self.turn_est = 0 
+    self.freq = 1
+    self.derTerm = 0
+    self.propTerm = 0
+    self.IntTerm = 0 
+
+    def __init__(self, first_value,frequency):
+        self.turn_est = first_value
+        self.freq = frequency
+    
+    def ComputeTurn(self, raw_command):
+        self.computeder(raw_command)
+        self.propTerm = raw_command
+        self.computeInt(raw_command)
+        self.turn_est = self.kd*self.derTerm + self.kp*self.propTerm + self.kI*self.IntTerm
+    
+    def computeder(self,raw_value):
+        return self.freq* (raw_value - self.turn_est)
+
+    def computeInt(self,raw_value):
+        self.IntTerm = self.IntTerm + raw_value
+        if self.IntTerm*self.IntTerm > self.kiMax:
+            self.IntTerm = 1
+
+    
