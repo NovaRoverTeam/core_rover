b2977bc8fc78fb64d1bd018c184bbc20af4349e5
jmcrobbie <jackmcrobbie@hotmail.com>
2019-05-08 14:52:29 +1000

    Debugging and tidying up

--- scripts/test.py
+++ scripts/test.py
@@ -1,10 +1,16 @@
 ## script to test the pid controller. 
-import steeringPID
+from steeringPID import steeringPID
+import time
 def pdiSteer(controller, rawCommand):
     controller.ComputeTurn(rawCommand)
+    print controller.turn_est 
     return controller.turn_est 
 target = 1
-
+state = 0
 pdiSteering = steeringPID(0,2)
-while(true):
-    print(pdiSteer(pdiSteering,target))
+while(1):
+    effort = pdiSteer(pdiSteering,target-state)
+    state = state + effort
+    print("effort was", effort)    
+    print("State is:",state)
+    time.sleep(0.1)
