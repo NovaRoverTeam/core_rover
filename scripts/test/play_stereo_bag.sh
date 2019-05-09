dab185a978e541edbf00a5e53c1cecf26d35529f
Nina <averill.nina@gmail.com>
2019-05-09 12:38:37 +1000

    Map builder outputting stereo odometry info

--- scripts/test/play_stereo_bag.sh
+++ scripts/test/play_stereo_bag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosbag play stereo_outdoor.bag /stereo_camera/left/camera_info_throttle:=/stereo/left/info /stereo_camera/right/camera_info_throttle:=/stereo/right/info /stereo_camera/left/image_raw_throttle/compressed:=/stereo/left/raw /stereo_camera/right/image_raw_throttle/compressed:=/stereo/right/raw
+rosbag play --clock stereo_outdoor.bag /stereo_camera/left/camera_info_throttle:=/stereo/left/info /stereo_camera/right/camera_info_throttle:=/stereo/right/info /stereo_camera/left/image_raw_throttle/compressed:=/stereo/left/raw /stereo_camera/right/image_raw_throttle/compressed:=/stereo/right/raw
