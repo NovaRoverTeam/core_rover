0b5f885b68f5aa5b6b9ca332b777da3ff9e7f6db
Evan <ecra93@gmail.com>
2019-05-15 18:56:47 +1000

    Node for publishing current objective as NavSatFix

---
+++ scripts/objective_node.py
@@ -0,0 +1,35 @@
+import rospy
+from sensor_msgs.msg import NavSatFix
+from nova_common.msg import *
+from nova_common.srv import *
+
+state_objective_latitude = None
+state_objective_longitude = None
+
+def is_ready():
+    return state_objective_latitude != None and\
+           state_objecctive_longitude != None
+
+def handle_receive_objective(req):
+    state_objective_latitude = req.latitude
+    state_objective_longitude = req.longitude
+
+def objective_node():
+
+    # node should update objective when notified by server
+    server = Rospy.Service("/core_rover/start_auto", StartAuto, handle_receive_objective)
+
+    # publish objective
+    objective_publisher = rospy.Publisher("/core_rover/current_objective", NavSatFix, queue_size=1)
+
+    rospy.init_node("objective_node")
+    publish_rate = rospy.Rate(2)
+    while not rospy.is_shutdown():
+        if is_ready():
+            msg = NavSatFix()
+            msg.latitude = state_objective_latitude
+            msg.longitude = state_objective_longitude
+            objective_publisher.publish(msg)
+        else:
+            rospy.logdebug("Objective node not ready to publish.")
+        publish_rate.sleep()
