bb18ed8225772b818f4b2ac172bc382c5f5c2c71
Emily Kuo <ekuo1@student.monash.edu>
2019-05-15 18:18:56 +1000

    Actually fixed tennis loc

--- scripts/tennis_loc.py
+++ scripts/tennis_loc.py
@@ -8,12 +8,13 @@ import time
 import socket
 import sys
 import os
+import subprocess
 from std_msgs.msg import String
 #--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--
 # getAutoMode(): Retrieve Mode from parameter server.
 #--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--**--..--
 def getAutoMode():
-    return rospy.get_param('/core_rover/autonomous_mode')
+    return rospy.get_param('/core_rover/autonomous_mode','On')
 def X_Zone(x):
     zones = ('left','centre','right')
     centre_border = (426,853)
@@ -34,14 +35,15 @@ def Drive_Forward(w,h,threshold):
     return avg<threshold
 
 def DriverMsg(data,steer_limit,rpm_limit):
-    steer = {'left':-steer_limit,'centre':0,'right':steer_limit}
+    steer = {'left':-steer_limit*50,'centre':0,'right':steer_limit*50}
     cmd = DriveCmd()
     cmd.rpm = 0
     direction = X_Zone(data[0])
-    rospy.loginfo(Drive_Forward(data[2],data[3],200))
+    forward_or_not = Drive_Forward(data[2],data[3],200)
+    rospy.loginfo(forward_or_not)
     cmd.steer_pct = steer[direction]
-    if (direction == 'centre' and Drive_Forward(data[2],data[3],200)):
-        cmd.rpm = rpm_limit
+    if (direction == 'centre' and forward_or_not):
+        cmd.rpm = rpm_limit*50
     else:
         cmd.rpm = 0
     return cmd
@@ -49,7 +51,7 @@ def DriverMsg(data,steer_limit,rpm_limit):
 def tennis_loc():
     rospy.set_param('steer_limit', 20)
     rospy.set_param('rpm_limit', 10)
-    server_address = '/home/nvidia/Documents/YOLO3-4-Py/mysocket'
+    server_address = '/tmp/tennis_loc_socket'
 	    # Make sure the socket does not already exist
     try:
         os.unlink(server_address)
@@ -57,8 +59,7 @@ def tennis_loc():
         if os.path.exists(server_address):
             raise
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sock.bind(server_address)
-    sock.listen(1)
+
     # ball_pub  = rospy.Publisher("/core_rover/navigation/tennis_loc", DriveCmd, queue_size=10)
     ball_pub  = rospy.Publisher("/core_rover/driver/drive_cmd", DriveCmd, queue_size=10)
     status_pub = rospy.Publisher("/core_rover/navigation/tennis_stat", String, queue_size=10)
@@ -67,15 +68,24 @@ def tennis_loc():
     rate_inactive = rospy.Rate(1)
     rospy.loginfo("tennis ball location node started")
     autonomous_mode = 'Off'
+    vision_process = subprocess.Popen(['python3', '/home/nvidia/Documents/NeuralNet/YOLO3-4-Py/V2_Foscam_2.py'])
+    sock.bind(server_address)
+    sock.listen(1)
+    connection, client_address = sock.accept()
+    sock.setblocking(0)
     while not rospy.is_shutdown():
-    	connection, client_address = sock.accept()
-    	while getAutoMode() != 'Off':
-          steer_limit = rospy.get_param('steer_limit')
-          rpm_limit   = rospy.get_param('rpm_limit')
+    	if getAutoMode() != 'Off': 
+
+          steer_limit = rospy.get_param('steer_limit',0.3)
+          rpm_limit   = rospy.get_param('rpm_limit',0.3)
+
           data = connection.recv(100) # 1 Byte per character
-          if data:
+          rospy.loginfo('cat4')
+          rospy.loginfo(type(data))
+          rospy.loginfo(data)
+          if data != '0':
               status_pub.publish("Found")
-              #rospy.loginfo(data)
+              rospy.loginfo("Found")
               data_parsed = ast.literal_eval(data)
               #print(data_parsed)
               #array = Float32MultiArray(4,data_parsed)
@@ -84,11 +94,12 @@ def tennis_loc():
               ball_pub.publish(msg)
           else:
               status_pub.publish("Lost")
-              connection.close()
-              break;
+              rospy.loginfo("lost")
           rate_active.sleep()
-        rate_inactive.sleep()
-
+    	else:
+          rate_inactive.sleep()
+    connection.close()
+    vision_process.terminate()
 
 if __name__ == '__main__':
     tennis_loc()
