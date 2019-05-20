dc873cb23bf7e533de770bab80acdee4702c185d
Nina A <averill.nina@gmail.com>
2019-05-20 17:31:09 +1000

    Added frame for lidar

--- scripts/test/play_stereo_bag.sh
+++ scripts/test/play_stereo_bag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosbag play --clock stereo_outdoor.bag /stereo_camera/left/camera_info_throttle:=/elp/left/camera_info /stereo_camera/right/camera_info_throttle:=/elp/right/camera_info /stereo_camera/left/image_raw_throttle/compressed:=/elp/left/image_raw/compressed /stereo_camera/right/image_raw_throttle/compressed:=/elp/right/image_raw/compressed
+rosbag play --clock stereo_outdoor.bag /stereo_camera/left/camera_info_throttle:=/elp/left/camera_info /stereo_camera/right/camera_info_throttle:=/elp/right/camera_info /stereo_camera/left/image_raw_throttle/compressed:=/elp/left/image_raw/compressed /stereo_camera/right/image_raw_throttle/compressed:=/elp/right/image_raw/compressed 
