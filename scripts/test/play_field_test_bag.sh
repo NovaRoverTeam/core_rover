6edb76e1c206825e8ae6020f8751e2c5b376618f
jmcrobbie <jackmcrobbie@hotmail.com>
2019-05-21 11:23:27 +1000

    Fized topics names to rover convention

--- scripts/test/play_field_test_bag.sh
+++ scripts/test/play_field_test_bag.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rosbag play field_test_ekf.bag /px4/raw/gps:=/nova_common/gps_data /px4/raw/imu:=/nova_common/IMU
+rosbag play field_test_latest.bag /px4/raw/gps:=/nova_common/gps_data /px4/raw/imu:=/nova_common/IMU
