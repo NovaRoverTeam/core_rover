577dcc73c292c3f9d709bb778b1f5dfe1a7cd8c6
Evan <ecra93@gmail.com>
2019-05-21 16:18:32 +1000

    add a_star unit testing

--- scripts/a_star.py
+++ scripts/a_star.py
@@ -17,7 +17,7 @@ def reconstruct_path_to_destination(prev, end):
     curr = end
     while curr in prev.keys():
         curr = prev[curr]
-        path.insert(curr, 0)
+        path.insert(0, curr)
     return path
 
 def edge_distance_between(node_from, node_to, grid):
@@ -27,7 +27,7 @@ def edge_distance_between(node_from, node_to, grid):
     the API for this function as is, however, in case our representation of the
     graph changes in future.
     """
-    return grid[n2[0]][n2[1]]
+    return grid[node_to[0]][node_to[1]]
 
 def neighbors_of(node, grid):
     """
@@ -44,7 +44,7 @@ def neighbors_of(node, grid):
             c = node[1] + c_delta
 
             # skip neighbors that would take us off the edge of the grid
-            if r < 0 or r >= nrows or c < 0 or c >= ncols:
+            if r < 0 or r >= n_rows or c < 0 or c >= n_cols:
                 continue
             # skip the current node itself
             if r_delta == 0 and c_delta == 0:
@@ -59,13 +59,13 @@ def node_with_min_fscore(open_set, f_cost):
     Find the cell in open set with the smallest f score.
     """
     # TODO: replace this nasty implementation with a heapq
-    max_score = -1
-    max_node = Node
+    min_score = 10e5
+    min_node = None
     for node in open_set:
-        if f_score[node] > max_score:
-            max_score = f_score[node]
-            max_node = node
-    return node
+        if f_cost[node] < min_score:
+            min_score = f_cost[node]
+            min_node = node
+    return min_node
 
 def a_star(grid, start, end, heuristic_cost=euclidean_heuristic_cost):
     """
@@ -98,13 +98,15 @@ def a_star(grid, start, end, heuristic_cost=euclidean_heuristic_cost):
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             cell = (r, c)
-            g_cost[cell] = float.inf("inf")
-            f_cost[cell] = float.inf("inf")
+            g_cost[cell] = float("Inf")
+            f_cost[cell] = float("Inf")
     g_cost[start] = 0
     f_cost[start] = heuristic_cost(start, end)
 
     while len(open_set) != 0:
         # node in open set with min fscore
+
+        # should shoul be f_cost + g_cos
         curr = node_with_min_fscore(open_set, f_cost)
         
         # if we've reached the destination
@@ -120,20 +122,107 @@ def a_star(grid, start, end, heuristic_cost=euclidean_heuristic_cost):
             if neighbor in closed_set:
                 continue
 
-            curr_g_score = g_score[curr] + distance_between(curr, neighbor, grid)
+            curr_g_cost = g_cost[curr] + edge_distance_between(curr, neighbor, grid)
 
             # add neighbor to newly discovered nodes
             if neighbor not in open_set:
                 open_set.add(neighbor)
 
             # if we've already got a lower g_score for neighbor, then move on
-            elif curr_g_score >= g_score[neighbor]:
+            elif curr_g_cost >= g_cost[neighbor]:
                 continue
 
             prev[neighbor] = curr
-            g_score[neighbor] = curr_g_score
-            f_score[neighbor] = g_score[neighbor] + heuristic_cost(neighbor, end)
+            g_cost[neighbor] = curr_g_cost
+            f_cost[neighbor] = g_cost[neighbor] + heuristic_cost(neighbor, end)
 
     # if we get to this point, it's not possible to reach the end destination
     return []
 
+if __name__ == "__main__":
+
+    eps = 10e-5
+
+    # test euclidean_heruistic_cost
+    test_cases = [
+        [(0,0), (1,1), sqrt(2)],
+        [(1,2), (8,7), sqrt(49+25)]
+    ]
+    for tc in test_cases:
+        assert(abs(euclidean_heuristic_cost(tc[0], tc[1]) - tc[-1] < eps))
+
+    # test reconstruct_path_to_destination
+    test_cases = [
+        [
+            {(0,1): (0,0), (1,1): (0,1), (1,2): (1,1)},
+            (1,2), 
+            [(0,0), (0,1), (1,1), (1,2)]
+        ],
+        [
+            {(1,1): (0,1), (2,1): (1,1), (3,1): (2,1)},
+            (3,1),
+            [(0,1), (1,1), (2,1), (3,1)]
+        ]
+    ]
+
+    for tc in test_cases:
+        assert(reconstruct_path_to_destination(tc[0], tc[1]) == tc[-1])
+
+    # test edge_distance_between
+    grid = [
+        [0, 0, 10e10, 25],
+        [0, 10, 0, 1],
+        [0, 0 , 0, 0],
+        [1, 2, 10, 0],
+        [2, 3, 5, 5]
+    ]
+    test_cases = [
+       [(1,1), (2,2), 0]
+    ]
+
+    for tc in test_cases:
+        assert(edge_distance_between(tc[0], tc[1], grid) == tc[-1])
+    
+    # test neighbors_of
+    grid = [
+        [0, 0, 10e10, 25],
+        [0, 10, 0, 1],
+        [0, 0 , 0, 0],
+        [1, 2, 10, 0],
+        [2, 3, 5, 5]
+    ]
+    test_cases = [
+        [(1,1), [(0,1), (1,0), (2,1), (1,2), (0,0), (0,2), (2,2), (2,0)]],
+        [(0,0), [(0,1), (1,0), (1,1)]]
+    ]
+
+    for tc in test_cases: 
+        assert(neighbors_of(tc[0], grid) == sorted(tc[-1]))
+
+    # test node_with_min_fscore
+    # start (0, 0), goal (2,2)
+    test_cases = [
+        [[(0,0)], {(0,0): sqrt(8)}, (0,0)],
+        [[(0,1), (1,0), (1,1)], {(0,1): sqrt(5), (1,0): sqrt(5), (1,1): 10 + sqrt(2)}, (0,1)]
+    ]
+
+    for tc in test_cases:
+        assert(node_with_min_fscore(tc[0], tc[1]) == tc[-1])
+
+    # test a_start
+    grid = [
+        [0, 0, 10e10, 25],
+        [0, 10, 0, 1],
+        [0, 0 , 0, 0],
+        [1, 2, 10, 0],
+        [2, 3, 5, 5]
+    ]
+
+    test_cases = [
+        [(0,0), (2,2), [(0,0), (0,1), (1,2), (2,2)]],
+        [(0,0), (2,1), [(0,0), (1,0), (2,1)]],
+        [(0,0), (3,3), [(0,0), (0,1), (1,2), (2,3), (3,3)]]
+    ]
+    
+    for tc in test_cases:
+        assert(a_star(grid, tc[0], tc[1]) == tc[-1])
